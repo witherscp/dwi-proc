cdf6e452e437ab899e564a48e119d2b9203c7be2
Price Withers <price.withers@nih.gov>
2023-05-10 16:36:02 -0400

    Clean script

--- scripts/_get_labels_1D.py
+++ scripts/_get_labels_1D.py
@@ -2,39 +2,8 @@
 
 #====================================================================================================================
 
-# Name:         _get_labels_1D.py
-
 # Author:       Braden Yang
 # Date:         05/12/20
-# Updated:      05/18/20
-
-# usage: _get_labels_1D.py [-h] [-a APPEND_LABTAB_STR]
-#                          orig_labtab_str outfile
-#                          [append_roi_idx [append_roi_idx ...]]
-
-# Get a 1D file of the original ROI labels, plus an added set of ROIs appended
-# to the end of the original
-
-# positional arguments:
-#   orig_labtab_str       string of the original ROI network's labeltable
-#                         (outputted by '3dinfo -labeltable ${roi}')
-#   outfile               path to write 1D file
-#   append_roi_idx        list of ROI indices in the append ROI network to
-#                         select for to be included in the 1D file; must appear
-#                         in the order that they will be renumbered
-
-# optional arguments:
-#   -h, --help            show this help message and exit
-#   -a APPEND_LABTAB_STR, --append_labtab_str APPEND_LABTAB_STR
-#                         string of the ROI network to append's labeltable
-#                         (outputted by '3dinfo -labeltable ${roi}')
-
-# Main outputs: outfile
-# Requirements: Python 3
-# Notes:        - Called in DTI_do_cat_subctx.sh, DTI_do_surf2vol.sh, and DTI_do_FS_roi.sh
-
-# Change log:
-# 	- BYY 05/18/20: made appending additional rows to 1D optional
 
 #====================================================================================================================
 
